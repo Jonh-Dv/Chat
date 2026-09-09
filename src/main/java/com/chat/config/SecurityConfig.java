734ad4669ba1a874848a60cc8499920d18dfcd8b
João Pedro <joaopedro192003@gmail.com>
2026-09-08 22:37:20 -0300

    ADICIONANDOD SECURITYFILTERCHAIN PARA SELECAO DE ROTAS PROTEGIDAS E ROTAS PUBLICAS

--- src/main/java/com/chat/config/SecurityConfig.java
+++ src/main/java/com/chat/config/SecurityConfig.java
@@ -2,13 +2,34 @@ package com.chat.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
-@Configuration 
+@Configuration
 public class SecurityConfig {
-    
-    @Bean public PasswordEncoder passwordEncoder(){
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable).sessionManagement(session -> session.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS)).authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                HttpMethod.POST,
+                                "/auth/cadastro")
+                        .permitAll().anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
