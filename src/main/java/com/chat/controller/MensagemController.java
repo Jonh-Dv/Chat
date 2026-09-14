88b63b7059b5b00f639aef02e8c7dec580a260ba
João Pedro <joaopedro192003@gmail.com>
2026-09-14 18:07:04 -0300

    ADICIONANDO BUSCA POR MENSAGENS JÁ ENVIADAS.

--- src/main/java/com/chat/controller/MensagemController.java
+++ src/main/java/com/chat/controller/MensagemController.java
@@ -1,7 +1,11 @@
 package com.chat.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +37,29 @@ public class MensagemController {
                 mensagem.getRemetente().getId(),
                 mensagem.getConteudo(),
                 mensagem.getDataEnvio());
+    }
+
+    @GetMapping("/buscar-mensagens/{idConversa}/{idEnviando}/{idRecebendo}")
+    public List<MensagemDTO> buscarMensagens(
+        @PathVariable Long idConversa,
+        @PathVariable Long idEnviando,
+        @PathVariable Long idRecebendo
+    ){
+        List<Mensagem> mensagens = mensagemService.buscarMensagensOuCriarConversa(idConversa, idEnviando, idRecebendo);
+
+        List<MensagemDTO> mensagensRetorno = new ArrayList<>();
+
+        for(Mensagem mensagem : mensagens){
+            MensagemDTO mensagemDtoProvisorio = new MensagemDTO();
+            mensagemDtoProvisorio.setConteudo(mensagem.getConteudo());
+            mensagemDtoProvisorio.setDataEnvio(mensagem.getDataEnvio());
+            mensagemDtoProvisorio.setId(mensagem.getId());
+            mensagemDtoProvisorio.setIdConversa(mensagem.getIdConversa().getIdConversa());
+            mensagemDtoProvisorio.setIdRemetente(mensagem.getRemetente().getId());
+
+            mensagensRetorno.add(mensagemDtoProvisorio);
+        }
+
+        return mensagensRetorno;
     }
 }
--- src/main/java/com/chat/dto/MensagemDTO.java
+++ src/main/java/com/chat/dto/MensagemDTO.java
@@ -2,10 +2,19 @@ package com.chat.dto;
 
 import java.time.LocalDateTime;
 
-public record MensagemDTO(
-    Long id,
-    Long idConversa,
-    Long idRemetente,
-    String conteudo,
-    LocalDateTime dataEnvio
-) {}
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MensagemDTO {
+    private Long id;
+    private Long idConversa;
+    private Long idRemetente;
+    private String conteudo;
+    private LocalDateTime dataEnvio;
+}
